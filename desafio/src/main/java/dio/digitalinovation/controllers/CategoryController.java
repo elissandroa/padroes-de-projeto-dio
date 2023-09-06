af08fc9f4436124a8d60fc613f07348934236ad0
elissandroa <elissandroa@hotmail.com>
2023-09-06 14:15:20 -0300

    Fix package services / Dependency SpringDoc OpenAPI

--- desafio/src/main/java/dio/digitalinovation/controllers/CategoryController.java
+++ desafio/src/main/java/dio/digitalinovation/controllers/CategoryController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dio.digitalinovation.CategoryService;
 import dio.digitalinovation.entities.Category;
+import dio.digitalinovation.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
--- desafio/src/main/java/dio/digitalinovation/services/CategoryService.java
+++ desafio/src/main/java/dio/digitalinovation/services/CategoryService.java
@@ -1,4 +1,4 @@
-package dio.digitalinovation;
+package dio.digitalinovation.services;
 
 import java.util.List;
 import java.util.Optional;

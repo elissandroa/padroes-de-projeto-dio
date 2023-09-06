4a369e269709103197b510c3326ba454abac5ea6
elissandroa <elissandroa@hotmail.com>
2023-09-06 11:41:38 -0300

    Start project

---
+++ desafio/src/main/java/dio/digitalinovation/LabDeafioDioPadroesApplication.java
@@ -0,0 +1,13 @@
+package dio.digitalinovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDeafioDioPadroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDeafioDioPadroesApplication.class, args);
+	}
+
+}
---
+++ desafio/src/test/java/dio/digitalinovation/LabDeafioDioPadroesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.digitalinovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDeafioDioPadroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
